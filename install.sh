1d1e2d8e945b13ce5e3598d73ee3247bfd97cfa3
milfeld <kent.milfeld@gmail.com>
2026-04-12 11:46:01 -0500

    removed debugging (premature)  exit frmm install.sh and include .gitignore

--- install.sh
+++ install.sh
@@ -29,7 +29,6 @@ USAGE2=" ->         Use functions normally, scripts if developing."
 
 [[ $1 != scripts ]] && [[ $1 != functions ]] &&  \
    echo " Error: did not understand $0 $1" && echo "$USAGE1" && exit 1
-exit
 
 #============
 
